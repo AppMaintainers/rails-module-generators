a4d9e7b34aadf237842317ddc67aa765d5c66da1
Attila Juhász <juhasz@atti.la>
2016-04-28 14:34:17 +0200

    first commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in rails-module-generators.gemspec
+gemspec
---
+++ lib/rails/module/generators.rb
@@ -0,0 +1,9 @@
+require "rails/module/generators/version"
+
+module Rails
+  module Module
+    module Generators
+      # Your code goes here...
+    end
+  end
+end
